3ad5a8c236cc3b18ae47fe3a4861fe5fbe66376d
Danny Gershman <danny.gershman@gmail.com>
2015-12-29 14:30:05 -0500

    Create config.js
    adding config

---
+++ config.js
@@ -0,0 +1,15 @@
+exports.settings = {
+    "bmltServerRoot":"http://<server-name>/main_server",
+    "nodeMailerTransportConfiguration": {
+        "service": "",
+        "auth" : {
+            user: '',
+            pass: ''
+        }
+    },
+    "nodeMailerMessageConfiguration": {
+        from: '',
+        to: '',
+        subject: 'Meetinglist CHANGE request'
+    }
+}
